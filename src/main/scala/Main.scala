3ef9b99efac2e59500c3e31c9a3b6856d8dd81e2
vladislav.sterkhov <vladislav.sterkhov@south.rt.ru>
2023-02-22 13:15:09 +0300

    migrate to local. easy way

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,26 +4,12 @@ import org.apache.hadoop.fs._
 import service.HDFSFileService
 import config.Constants.Hadoop._
 
-import java.io.File
-import scala.util.Using
-
 object Main {
 
   def main(args: Array[String]): Unit = {
     val hdfsFileService = new HDFSFileService()
 
-    hdfsFileService.removeFile(STAGE_PATH)
     hdfsFileService.removeFile(ODS_PATH)
-
-    val tarPath = new Path("/stage.tar.gz")
-
-    Using(getClass.getResource(tarPath.toString).openStream()) { stream =>
-      hdfsFileService.writeContent(stream, tarPath)
-    }
-
-    val tar = hdfsFileService.unGz(tarPath)
-
-    FileUtil.unTar(hdfsFileService.openInputStream(tar), new File("/"), false)
 
     for (dir <- hdfsFileService.getDirectories(STAGE_PATH)){
       val stageFiles = hdfsFileService.getFilesWithExtension(dir, "csv")
@@ -32,7 +18,7 @@ object Main {
         val odsFile = new Path(odsDir + "/result.csv")
         hdfsFileService.mkDir(odsDir)
         stageFiles.foreach(sf=>{
-          hdfsFileService.writeContent(hdfsFileService.openInputStream(sf), odsFile)
+          hdfsFileService.writeContent(hdfsFileService.openInputStream(sf), odsFile, true)
         })
       }
     }
--- src/main/scala/service/HDFSFileService.scala
+++ src/main/scala/service/HDFSFileService.scala
@@ -4,18 +4,13 @@ package service
 import org.apache.hadoop.conf._
 import org.apache.hadoop.fs._
 import org.apache.hadoop.io.IOUtils
-import org.apache.hadoop.io.compress.CompressionCodecFactory
 import java.io.{InputStream}
 import scala.collection.mutable.ListBuffer
 import scala.util.Using
 
 class HDFSFileService {
   protected val conf = new Configuration()
-  //conf.addResource(new Path("hdfs-site.xml"))
-  //conf.addResource(new Path("core-site.xml"))
-  //conf.set("fs.hdfs.impl", classOf[DistributedFileSystem].getName)
-  //conf.set("fs.file.impl", classOf[LocalFileSystem].getName)
-  System.setProperty("HADOOP_USER_NAME", "hdfs")
+  System.setProperty("HADOOP_USER_NAME", "toor")
   System.setProperty("hadoop.home.dir", "/")
 
   protected val fileSystem: FileSystem = FileSystem.get(conf)
@@ -49,7 +44,10 @@ class HDFSFileService {
     buffer.toList
   }
 
-  def writeContent(is: InputStream, outPath: Path): Unit = {
+  def writeContent(is: InputStream, outPath: Path, append: Boolean): Unit = {
+    if (fileSystem.exists(outPath) && !append)
+      throw new FileAlreadyExistsException("File exists with path - "+outPath)
+
     Using.Manager { use =>
       val out: FSDataOutputStream = use(if (fileSystem.exists(outPath)) fileSystem.append(outPath) else fileSystem.create(outPath))
       IOUtils.copyBytes(is, out, conf)
@@ -59,20 +57,5 @@ class HDFSFileService {
   def removeFile(distPath: Path): Unit = {
     if (fileSystem.exists(distPath))
       fileSystem.delete(distPath, true)
-  }
-
-  def unGz(archPath: Path): Path = {
-    val factory = new CompressionCodecFactory(conf)
-    val codec = factory.getCodec(archPath)
-
-    if (codec == null) {
-      print("No codec found for " + archPath)
-      return null
-    }
-
-    val outPath = new Path(CompressionCodecFactory.removeSuffix(archPath.toString, codec.getDefaultExtension))
-    val is = codec.createInputStream(openInputStream(archPath))
-    writeContent(is, outPath)
-    outPath
   }
 }
